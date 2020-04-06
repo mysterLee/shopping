ba6a663e82cd7ac1042c10a0615eb307f7c9ba28
mysterLee <61910420+mysterLee@users.noreply.github.com>
2020-04-06 10:00:50 +0900

    최신

--- shopping/real.py
+++ shopping/real.py
@@ -1,44 +1,45 @@
 from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
 
-from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
+
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
     return render_template('index.html')
 
-## API 역할을 하는 부분
-## API 역할을 하는 부분
-@app.route('/reviews', methods=['POST'])
-def order_purchse():
 
+## API 역할을 하는 부분
+@app.route('/orders', methods=['POST'])
+def write_order():
     name_receive = request.form['name_give']
-    count_receive = request.form['count_give']
-    address_receive = request.form['address_give']
-    phone_receive = request.form['phone_give']
+    option_receive = request.form['option_give']
+    add_receive = request.form['add_give']
+    tel_receive = request.form['tel_give']
 
-    # DB에 삽입할 review 만들기
     order = {
         'name': name_receive,
-        'count': count_receive,
-        'address': address_receive,
-        'phone': phone_receive
+        'option': option_receive,
+        'add': add_receive,
+        'tel': tel_receive
     }
-    # reviews에 review 저장하기
-    db.orders.insert_one(order)
-	# 3. 성공 여부 & 성공 메시지 반환하기
-    return jsonify({'result': 'success', 'msg': '성공적으로 주문되었습니다.'})
+
+    db.order_get.insert_one(order)
+    return jsonify({'result': 'success', 'msg': '주문이 성공적으로 되었습니다.'})
 
 
-@app.route('/oredrs', methods=['GET'])
-def read_reviews():
-        # 1. DB에서 오더 정보 모두 가져오기
-        reviews = list(db.orders.find({}, {'_id': 0}))
-        # 2. 성공 여부 & 리뷰 목록 반환하기
-        return jsonify({'result': 'success', 'orders': orders})
+@app.route('/orders', methods=['GET'])
+def get_order():
+    orders = list(db.order_get.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'order_get': orders})
+
 
 if __name__ == '__main__':
     app.run('127.0.0.1', port=5000, debug=True)
+
+# from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
